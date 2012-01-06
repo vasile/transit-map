06b4abc159f1372717e656db6ce531ca3c8d59ce
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2012-01-06 20:29:54 +0100

    Fixed scope, imagesPool belongs to vehicle_helpers only

--- static/js/map.js
+++ static/js/map.js
@@ -145,32 +145,6 @@ $(document).ready(function(){
         };
     })();
 
-    // Vehicle icons manager. 
-    // Roles:
-    // - keep a reference for each vehicle type (IC, ICE, etc..)
-    var imagesPool = (function(){
-        var icons = {};
-        function iconGet(type) {
-            if (typeof icons[type] !== 'undefined') {
-                return icons[type];
-            }
-
-            var icon = new google.maps.MarkerImage(
-                'static/images/vehicle-types/' + type + '.png',
-                 new google.maps.Size(20, 20),
-                 new google.maps.Point(0, 0),
-                 new google.maps.Point(10, 10)
-            );
-            icons[type] = icon;
-
-            return icon;
-        }
-        
-        return {
-            iconGet: iconGet
-        };
-    })();
-    
     // Routes manager.
     // Roles:
     // - keep a reference for the routes between stations
@@ -484,6 +458,34 @@ $(document).ready(function(){
     // - check backend for new vehicles
     // - manages vehicle objects(class Vehicle) and animates them (see Vehicle.render method)
     var vehicle_helpers = (function(){
+
+        // Vehicle icons manager. 
+        // Roles:
+        // - keep a reference for each vehicle type (IC, ICE, etc..)
+        var imagesPool = (function(){
+            var icons = {};
+            function iconGet(type) {
+                if (typeof icons[type] !== 'undefined') {
+                    return icons[type];
+                }
+
+                var icon = new google.maps.MarkerImage(
+                    'static/images/vehicle-types/' + type + '.png',
+                     new google.maps.Size(20, 20),
+                     new google.maps.Point(0, 0),
+                     new google.maps.Point(10, 10)
+                );
+                icons[type] = icon;
+
+                return icon;
+            }
+
+            return {
+                iconGet: iconGet
+            };
+        })();
+        
+        
         var vehicle_ib = new InfoBox({
             disableAutoPan: true,
             pixelOffset: new google.maps.Size(10, 10),
